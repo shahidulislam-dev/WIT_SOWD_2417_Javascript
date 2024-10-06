0decd3d36fed138e1d77d6a723666f68e75db088
Shahidul Islam <shahidul2562@gmail.com>
2024-10-07 03:18:10 +0600

    javascript class 1 problems

---
+++ Class_1/problems.js
@@ -0,0 +1,100 @@
+//Problem 1
+let sarasStoredApples = 25;
+let sarasBuyingApples = 15;
+
+let sarasTotalApple = sarasStoredApples + sarasBuyingApples;
+
+let answer1 = 'Total number of apples are: ' + sarasTotalApple;
+console.log(answer1);
+
+//Problem 2
+let jonsHadMoney = 500;
+let jonsSpendMoney = 200;
+
+let jonsPresentMoney = jonsHadMoney - jonsSpendMoney;
+
+let answer2 = 'Now Jons have' + ' ' + jonsPresentMoney + ' ' + 'taka.';
+console.log(answer2);
+
+//Problem 3
+let productionInHour = 120;
+let givenHour = 8;
+
+let totalProductionInGivenHour = productionInHour * givenHour;
+let answer3 = 'Make ' + totalProductionInGivenHour + ' machine in 8 hours: ';
+console.log(answer3);
+
+// problem 4
+let treesPerRow = 12;
+let totalRows = 8;
+
+let totalTreesInEightRows = treesPerRow * totalRows;
+
+let extraPlantingTrees = 20;
+let totalTreesInGarden = totalTreesInEightRows + extraPlantingTrees;
+
+let answer4 = 'Total trees in the garden is: ' + totalTreesInGarden;
+console.log(answer4);
+
+
+//Problem 5
+let rahimsBoxes = 5;
+let orrangePerBox = 10;
+
+let totalOrrangeOfRahim = rahimsBoxes * orrangePerBox;
+
+let givingOrranges = 12;
+
+let nowRahimsOrranges = totalOrrangeOfRahim - givingOrranges;
+let answer5 = "Now Rahim's have " + nowRahimsOrranges + ' ' + "Orranges.";
+console.log(answer5);
+
+
+//Problem 6
+let totalDistance = 150;
+let timeToAccrossDistance = 3;
+
+let expectedDistanceInOneHour = totalDistance / timeToAccrossDistance;
+let answer6 = 'The train accross' + ' ' + expectedDistanceInOneHour + ' ' + 'km in one hour.';
+console.log(answer6);
+
+
+
+//Problem 7
+let numberOfMechine = 6;
+let production = 200;
+
+//Equation for finding the production for per mechine
+let productionByPerMechine = production / numberOfMechine;
+
+let newMechineAdded = 2;
+let totalMechine = numberOfMechine + newMechineAdded;
+
+//Equation for finding total production.
+let totalProductionByAllMechine = productionByPerMechine * totalMechine;
+let answer7 = "Total product production = " + totalProductionByAllMechine.toFixed(2) + " and unsold product = " + 3;
+console.log(answer7);
+
+
+
+
+//Problem 8
+let totalChocolate = 120;
+let chocolatePerPacket = 9;
+
+let remainingChocolate = totalChocolate % chocolatePerPacket;
+let answer8 = 'The shopkeeper has ' + remainingChocolate + ' chocolates left';
+console.log(answer8);
+
+
+
+
+
+
+
+  
+
+
+
+
+
